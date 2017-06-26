5bf68801b904db73fdfd4e9d13895c1a4593ef31
Fabian Bächli <fb@spline.ch>
2017-06-26 13:34:25 +0200

    Changes

--- gui/main_winow_controller.java
+++
@@ -1,7 +0,0 @@
-public class main_window_controller implements Initializable {
-   // TODO: Import all Objects from fxml file
-    @Override
-    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-      // TODO: Handle UI Logic
-    }
-}
---
+++ src/sample/Controllers/MainWindowController.java
@@ -0,0 +1,15 @@
+package sample.Controllers;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable {
+   // TODO: Import all Objects from fxml file
+
+    @Override
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+      // TODO: Handle UI Logic
+    }
+
+}
