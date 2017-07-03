d90f004d20e4de516032050833f5b5263a8770dc
Fabian Bächli <fb@spline.ch>
2017-07-03 13:16:22 +0200

    removed workspace xml

--- src/sample/Controllers/MainWindowController.java
+++ src/sample/Controllers/MainWindowController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import sample.Models.Saal;
